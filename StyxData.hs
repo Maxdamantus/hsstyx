ca547fc9a1972cbb9704c54af67b72bb6a3bf0a4
Max Zerzouri <maxdamantus@gmail.com>
2015-02-02 02:26:51 +1300

    Some partial encoding/decoding.

--- StyxData.hs
+++ StyxData.hs
@@ -1,66 +1,109 @@
 module StyxData where
 
-import Data.Functor
+import Control.Applicative
 import Data.Word
 import Data.ByteString
 import Data.Binary
+import Data.Binary.Get
+import Data.Binary.Put
+
+-- not using Binary class, should make lists/ByteStrings/words (le) easier
+class SBinary t where
+  sput :: t -> Put
+  sget :: Get t
+
+instance SBinary Word8 where
+  sput = putWord8
+  sget = getWord8
+
+instance SBinary Word16 where
+  sput = putWord16le
+  sget = getWord16le
+
+instance SBinary Word32 where
+  sput = putWord32le
+  sget = getWord32le
+
+instance SBinary Word64 where
+  sput = putWord64le
+  sget = getWord64le
+
+instance (SBinary a, SBinary b) => SBinary (a, b) where
+  sput (a, b) = sput a >> sput b
+  sget = (,) <$> sget <*> sget
+
+instance (SBinary a, SBinary b, SBinary c) => SBinary (a, b, c) where
+  sput (a, b, c) = sput a >> sput b >> sput c
+  sget = (,,) <$> sget <*> sget <*> sget
+
+instance (SBinary a, SBinary b, SBinary c, SBinary d) => SBinary (a, b, c, d) where
+  sput (a, b, c, d) = sput a >> sput b >> sput c >> sput d
+  sget = (,,,) <$> sget <*> sget <*> sget <*> sget
+
+instance SBinary ByteString where
+  sput bs = sput (fromIntegral $ Data.ByteString.length bs :: Word16) >> putByteString bs
+  sget = do{ len <- get; getByteString (fromIntegral (len :: Word16)) }
+
+instance SBinary a => SBinary [a] where
+  sput l = sput (fromIntegral $ Prelude.length l :: Word16) >> mapM_ sput l
+  sget = do{ len <- get; sequence $ Prelude.replicate (fromIntegral (len :: Word16)) sget }
 
 newtype Fid = Fid Word32
   deriving (Show, Read, Eq, Ord)
 
-instance Binary Fid where
-  put (Fid w) = put w
-  get = Fid <$> get
+instance SBinary Fid where
+  sput (Fid w) = sput w
+  sget = Fid <$> sget
 
 newtype Tag = Tag Word32
   deriving (Show, Read, Eq)
 
-instance Binary Tag where
-  put (Tag w) = put w
-  get = Tag <$> get
+instance SBinary Tag where
+  sput (Tag w) = sput w
+  sget = Tag <$> sget
 
 newtype Mode = Mode Word8
   deriving (Show, Read, Eq)
 
-instance Binary Mode where
-  put (Mode w) = put w
-  get = Mode <$> get
+instance SBinary Mode where
+  sput (Mode w) = sput w
+  sget = Mode <$> sget
 
 newtype Perm = Perm Word32
   deriving (Show, Read, Eq)
 
-instance Binary Perm where
-  put (Perm w) = put w
-  get = Perm <$> get
+instance SBinary Perm where
+  sput (Perm w) = sput w
+  sget = Perm <$> sget
 
 newtype Type = Type Word8
   deriving (Show, Read, Eq)
 
-instance Binary Type where
-  put (Type w) = put w
-  get = Type <$> get
+instance SBinary Type where
+  sput (Type w) = sput w
+  sget = Type <$> sget
 
 newtype Version = Version Word32
   deriving (Show, Read, Eq)
 
-instance Binary Version where
-  put (Version w) = put w
-  get = Version <$> get
+instance SBinary Version where
+  sput (Version w) = sput w
+  sget = Version <$> sget
 
 newtype Path = Path Word64
   deriving (Show, Read, Eq)
 
-instance Binary Path where
-  put (Path w) = put w
-  get = Path <$> get
+instance SBinary Path where
+  sput (Path w) = sput w
+  sget = Path <$> sget
 
 data Qid = Qid Type Version Path
   deriving (Show, Read, Eq)
 
-instance Binary Qid where
-  put (Qid t v p) = put (t, v, p)
-  get = do
-    (t, v, p) <- get
+instance SBinary Qid where
+  sput (Qid t v p) = sput (t, v, p)
+  sget = do
+    (t, v, p) <- sget
     return (Qid t v p)
 
 data Tmessage =
@@ -72,22 +115,38 @@ data Tmessage =
   Topen Fid Mode |
   Tcreate Fid ByteString Perm Mode |
   Tread Fid Word64 Word32
+  -- TODO: Twrite, Tclunk, Tremove, Tstat, Twstat
   deriving (Show, Read)
 
 data TtaggedMessage = TtaggedMessage Tag Tmessage
   deriving (Show, Read)
 
-instance Binary TtaggedMessage where
-  put (TtaggedMessage tag msg) =
-    case msg of
-      Tversion msize version -> put (100 :: Word16, tag, msize, version)
-      Tauth afid uname aname -> put (102 :: Word16, afid, uname, aname)
-  get = do
-    msgType <- get
-    tag <- get
+instance SBinary TtaggedMessage where
+  sput (TtaggedMessage tag msg) = do
+    sput (msgType :: Word16)
+    putMsg
+    where
+      (msgType, putMsg) = case msg of
+        Tversion msize version -> (100, sput (tag, msize, version))
+        Tauth afid uname aname -> (102, sput (afid, uname, aname))
+        Tflush oldtag -> (104, sput oldtag)
+        Tattach fid afid uname aname -> (106, sput (fid, afid, uname, aname))
+        Twalk fid newfid nwnames -> (108, sput (fid, newfid, nwnames))
+        Topen fid mode -> (110, sput (fid, mode))
+        Tcreate qid name perm mode -> (112, sput (qid, name, perm, mode))
+        Tread fid offset count -> (114, sput (fid, offset, count))
+  sget = do
+    msgType <- sget
+    tag <- sget
     msg <- case msgType :: Word16 of
-      100 -> do{ (msize, version) <- get; return $ Tversion msize version }
-      102 -> do{ (afid, uname, aname) <- get; return $ Tauth afid uname aname }
+      100 -> do{ (msize, version) <- sget; return $ Tversion msize version }
+      102 -> do{ (afid, uname, aname) <- sget; return $ Tauth afid uname aname }
+      104 -> do{ oldtag <- sget; return $ Tflush oldtag }
+      106 -> do{ (fid, afid, uname, aname) <- sget; return $ Tattach fid afid uname aname }
+      108 -> do{ (fid, newfid, nwnames) <- sget; return $ Twalk fid newfid nwnames }
+      110 -> do{ (fid, mode) <- sget; return $ Topen fid mode }
+      112 -> do{ (qid, name, perm, mode) <- sget; return $ Tcreate qid name perm mode }
+      114 -> do{ (fid, offset, count) <- sget; return $ Tread fid offset count }
     return $ TtaggedMessage tag msg
 
 data Rmessage =
@@ -100,6 +159,7 @@ data Rmessage =
   Ropen Qid Word32 |
   Rcreate Qid Word32 |
   Rread ByteString
+  -- TODO: Rwrite, Rclunk, Rremove, Rstat, Rwstat
   deriving (Show, Read)
 
 data RtaggedMessage = RtaggedMessage Tag Rmessage
--- styx.hs
+++ styx.hs
@@ -8,56 +8,17 @@ import Data.ByteString
 import qualified Data.Map as M
 import qualified Data.ByteString.UTF8 as U
 
-newtype Fid = Fid Word32
-  deriving (Show, Read, Eq, Ord)
-
-newtype Tag = Tag Word32
-  deriving (Show, Read, Eq)
-
-newtype Mode = Mode Word8
-  deriving (Show, Read, Eq)
-
-newtype Perm = Perm Word32
-  deriving (Show, Read, Eq)
-
-newtype Type = Type Word8
-  deriving (Show, Read, Eq)
-
-newtype Version = Version Word32
-  deriving (Show, Read, Eq)
-
-newtype Path = Path Word64
-  deriving (Show, Read, Eq)
-
-data Qid = Qid Type Version Path
-  deriving (Show, Read, Eq)
-
-data Tmessage =
-  Tversion Word32 ByteString |
-  Tauth Fid ByteString ByteString |
-  Tflush Tag |
-  Tattach Fid Fid ByteString ByteString |
-  Twalk Fid Fid [ByteString] |
-  Topen Fid Mode |
-  Tcreate Fid ByteString Perm Mode |
-  Tread Fid Word64 Word32
-  deriving (Show, Read)
-
-data Rmessage =
-  Rversion Word32 ByteString |
-  Rauth Qid |
-  Rerror ByteString |
-  Rflush |
-  Rattach Qid |
-  Rwalk [Qid] |
-  Ropen Qid Word32 |
-  Rcreate Qid Word32 |
-  Rread ByteString
-  deriving (Show, Read)
+import StyxData
 
 type Err m = String -> StyxT m ()
 type Resp m t = t -> StyxT m ()
 type Handler m i o = i -> Err m -> Resp m o -> StyxT m ()
+
+{-
+writeRmessage :: ((Tag, Rmessage) -> ByteString)
+writeRmessage (tag, rmsg) =
+  ..
+  -}
 
 notYetImpl :: Handler m i o
 notYetImpl _ err _ = err "not yet implemented"
@@ -66,11 +27,13 @@ data FidHandler m = FidHandler {
   fhCreate :: Handler m (ByteString, Perm, Mode) (Qid, Word32),
   fhRead :: Handler m (Word64, Word32) ByteString,
   fhWrite :: Handler m (Word64, ByteString) Word32,
-  fhRemove :: Handler m () ()
+  fhRemove :: Handler m () (),
+  fhWalk :: Handler m [ByteString] [Qid],
+  fhOpen :: Handler m Mode (Qid, Word32)
 }
 
 nilFidHandler :: FidHandler m
-nilFidHandler = FidHandler n n n n
+nilFidHandler = FidHandler n n n n n n
   where n = notYetImpl
 
 data SrvHandler m = SrvHandler {
@@ -84,15 +47,23 @@ input sh out (tag, tmsg) = case tmsg of
   Tversion msize version ->
     resp $ Rversion msize version
   -- Tauth
-  -- Tflush
+  -- Tflush, somehow—doesn't really fit
   Tattach fid afid uname aname -> checkNoFid fid $ do
     modify $ M.insert fid Nothing
     shAttach sh (uname, aname) err $ \(qid, fh) -> do
       modify $ M.insert fid $ Just fh -- what if the client is bad?
       resp $ Rattach qid
-  -- Twalk
-  -- Topen
-  -- Tcreate
+  Twalk fid newfid wnames -> checkFid fid $ \fh ->
+    (if fid /= newfid then checkNoFid newfid else id) $ do
+      modify $ M.insert newfid $ Nothing
+      fhWalk fh wnames err $ resp . Rwalk
+  Topen fid mode -> checkFid fid $ \fh ->
+    fhOpen fh mode err $ \(qid, iounit) ->
+      resp $ Ropen qid iounit
+  Tcreate fid name perm mode -> checkFid fid $ \fh ->
+    fhCreate fh (name, perm, mode) err $ \(qid, iounit) -> do
+      -- TODO: replace fid
+      resp $ Rcreate qid iounit
   Tread fid offs len -> checkFid fid $ \fh ->
     fhRead fh (offs, len) err $ resp . Rread
   where
